9d294aa06d6efd220e69d35bd166076054172479
justice sowah <justice.sowah18@gmail.com>
2021-06-14 21:03:52 +0000

    installed nodemon, express. Created server using express. Created diff. request handlers & routes

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-const http = require('http');
+const express = require('express');
 
 const handleAllRequests = (requestObject, responseObject) => {
     console.log('Hi, I just received a request');
     const url = requestObject.url;
-    if (url == '/') {
-        responseObject.write('<html><body style="background-color:cyan; color:white"><h1>Welcome to the homepage <br/> There is no hurry in life <br/> Just keep trying</h1><br/></body></html> ');
-    }else if (url == '/login'){
-        responseObject.write('<html><body style="background-color:red; color:white"><h1>Welcome to the login page</h1></body></html>\n');
-    }else if (url == '/signup'){
-        responseObject.write('<html><body style="background-color:red; color:white"><h1>Welcome to the sign up page</h1></body></html>\n');
-    }else {
-        responseObject.write('<html><body style="background-color:red; color:white"><h1>Error 404, Page not found</h1></body></html>\n');
-    }
+    responseObject.setHeader("content-type","text/html");
+    
     responseObject.end();
 }
 
-const server = http.createServer(handleAllRequests);
+const handleHomeRequest = (req, res) => {
+    res.send('<h1>Welcome to the homepage <br/> There is no hurry in life <br/> Just keep trying</h1>');
+}
+
+const handleLoginRequest = (req, res) => {
+    res.send('<h1>Welcome to the Login page</h1>');
+}
+
+const handleSignUpRequest = (req, res) => {
+    res.send('<h1>Welcome to the Sign Up page</h1>');
+}
+
+const handleProfileRequest = (req, res) => {
+    res.send('<h1>Welcome to the Profile page</h1>');
+}
+
+const server = express();
+
+server.use('/login', handleLoginRequest);
+
+server.use('/signup', handleSignUpRequest);
+
+server.use('/profile', handleProfileRequest);
+
+server.use('/', handleHomeRequest);
 
 server.listen(3000, '127.0.0.1', () => console.log('Server is ready to accept requests'));
